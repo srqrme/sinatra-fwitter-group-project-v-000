415b5c81710b5f1f89cb35b7c809cbcdc5a9c24e
Sharmaine Requerme <srqrme@gmail.com>
2019-01-23 20:09:50 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,41 @@ class UsersController < ApplicationController
   get '/signup' do
     unless session[:user_id]
       erb :'users/signup'
+    else
+      redirect to "/tweets"
+    end
+  end
+
+#Creates new user and logs them in on valid submission
+#Does not let a logged in user view the signup page
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    end
+  end
+
+#Loads the login page
+  get '/login' do
+    unless session[:user_id]
+      erb :'users/login'
+    else
+      redirect to "/tweets"
+    end
+  end
+
+#Loads tweets index after login
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate([params[:password]])
+      session[:user_id] = user.id
+      redirect "/tweets"
+    else
+      redirect '/signup'
     end
   end
 
