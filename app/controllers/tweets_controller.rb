9a1ad740e75bd7678ad5c966360bc67deb92ef78
Sharmaine Requerme <srqrme@gmail.com>
2019-01-24 20:25:12 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,4 +17,14 @@ class TweetsController < ApplicationController
     end
   end
   
+  post '/tweets' do
+    if params[:content] == ""
+      redirect to '/tweets/new'
+    else
+      user = User.find_by_id(session[:user_id])
+      @tweet = Tweet.create(:content => params[:content], :user_id => user.id)
+      redirect to '/tweets/#{@tweet.id}'
+    end
+  end
+  
 end
