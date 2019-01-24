da063a79bd38547aa89e2fc46ec49ffff9bbfa2b
Sharmaine Requerme <srqrme@gmail.com>
2019-01-24 20:18:22 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,11 +2,19 @@ class TweetsController < ApplicationController
 
   get '/tweets' do
     if session[:user_id]
+      @tweets = Tweet.all
       erb :'tweets/index'
     else 
       redirect to '/login'
     end
   end
   
+  get '/tweets/new' do
+    if session[:user_id]
+      erb :'tweets/new_tweet'
+    else
+      redirect to '/login'
+    end
+  end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,9 +37,9 @@ class UsersController < ApplicationController
   end
   
   post '/login' do
-    @user = User.find_by(username: params["username"], password: params["password"])
-    if @user
-      session[:id] = @user.id
+    user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
       redirect to '/tweets'
     else
       redirect to '/login'
